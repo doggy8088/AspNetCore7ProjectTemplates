a556c3f61716ab9280465a9672313ba06e782089
Will <doggy.huang@gmail.com>
2022-12-16 02:00:45 +0800

    示範 ASP.NET Core gRPC Service 專案範本

---
+++ ASP.NET Core gRPC Service/Services/GreeterService.cs
@@ -0,0 +1,22 @@
+using ASP.NET_Core_gRPC_Service;
+using Grpc.Core;
+
+namespace ASP.NET_Core_gRPC_Service.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
