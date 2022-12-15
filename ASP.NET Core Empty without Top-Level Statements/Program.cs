3d721f78b44177a6f0a49b08b9534a587e8bb655
Will <doggy.huang@gmail.com>
2022-12-16 01:52:09 +0800

    示範 ASP.NET Core Empty 專案範本

---
+++ ASP.NET Core Empty without Top-Level Statements/Program.cs
@@ -0,0 +1,15 @@
+namespace ASP.NET_Core_Empty_without_Top_Level_Statements
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
