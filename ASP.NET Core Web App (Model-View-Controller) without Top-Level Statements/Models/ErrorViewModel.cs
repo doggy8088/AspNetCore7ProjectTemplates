767464af573699e2674486c7e37c20d18a38395d
Will <doggy.huang@gmail.com>
2022-12-18 12:21:53 +0800

    刪除有問題的 ASP.NET Core Web App (Model-View-Controller) without Top-Level Statements 專案
    因為專案名稱的關係，導致 MSBuild 找不到檔案路徑，會導致 VS 編譯失敗。

--- ASP.NET Core Web App (Model-View-Controller) without Top-Level Statements/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ASP.NET_Core_Web_App__Model_View_Controller__without_Top_Level_Statements.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
